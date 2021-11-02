e58b48353092317dbf09988d12da161f34c6028b
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-11-03 09:15:10 +1300

    add: GET /puzzles route

--- server/build/app.js
+++ server/build/app.js
@@ -1,19 +1,65 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
-var getSolutions = require("./lib/getSolutions");
+var _a = require("./lib/createPuzzles"), getRanSolutionsMongo = _a.getRanSolutionsMongo, createPuzzleSolutionSets = _a.createPuzzleSolutionSets;
+var cors = require("cors");
+app.use(cors());
 app.use(express.json());
 // test route
 app.get("/", function (req, res) {
     res.send("Hello World!");
 });
 // GET 10 puzzles for each difficulty, send to the client and to be saved in local storage
-app.get("/puzzles", function (req, res) {
-    // 1. get 10 solutions from the data folder
-    var solutions = getSolutions(10);
-    // 2. create puzzles based on the solutions
-    // 3. send the puzzles & solutions to the client
-    res.send("sudoku solutions are coming!");
-});
+app.get("/puzzles", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var solutions, result;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, getRanSolutionsMongo(10)];
+            case 1:
+                solutions = _a.sent();
+                console.log(solutions);
+                result = createPuzzleSolutionSets(solutions);
+                // 3. send the puzzles & solutions to the client
+                res.send({ solutions: result });
+                return [2 /*return*/];
+        }
+    });
+}); });
 module.exports = app;
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
 const express = require("express");
 const app = express();
-const getSolutions = require("./lib/getSolutions");
+const {
+  getRanSolutionsMongo,
+  createPuzzleSolutionSets,
+} = require("./lib/createPuzzles");
+const cors = require("cors");
 
+app.use(cors())
 app.use(express.json());
 
 // test route
@@ -11,15 +16,26 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 // GET 10 puzzles for each difficulty, send to the client and to be saved in local storage
-app.get("/puzzles", (req: Request, res: Response) => {
+app.get("/puzzles", async (req: Request, res: Response) => {
   // 1. get 10 solutions from the data folder
-  const solutions = getSolutions(10);
+  // let solutions: number[] = [];
+  // const count = await SolutionModel.count();
+
+  // for (let i = 0; i < 10; i++) {
+  //   var ranNum = Math.floor(Math.random() * count);
+  //   console.log("COUNT:", count, ranNum);
+  //   const { solution } = await SolutionModel.findOne().skip(ranNum);
+  //   solutions.push(solution);
+  // }
+  const solutions = await getRanSolutionsMongo(10);
+  console.log(solutions);
 
   // 2. create puzzles based on the solutions
+  const result = createPuzzleSolutionSets(solutions);
 
   // 3. send the puzzles & solutions to the client
 
-  res.send("sudoku solutions are coming!");
+  res.send({ solutions: result });
 });
 
 module.exports = app;
