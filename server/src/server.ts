441c8e248b4d3f5f34194af3d176445437cca75d
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-11-08 09:59:05 +1300

    add: env vars for deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 const app = require('./app')
 const {connect} = require('mongoose')
 
+const mongoDbUrl = process.env.MONGO_URI || 'mongodb://localhost:27017/test'
+
 connect("mongodb://localhost:27017/sudoku");
 
 const port = process.env.PORT || 5005
